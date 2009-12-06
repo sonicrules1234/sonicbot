113fdd9add80ec59295bd5d0fa7899890d2e365e
Westly Ward <westly@westly-laptop.(none)>
2009-12-06 13:02:38 -0800

    Added the licenses for PyAIML, PyYAML, and simplejson.  License for twitter.py is inside the file, and there was no license for the google translate file

---
+++ plugins/whois.py
@@ -0,0 +1,9 @@
+arguments = ["self", "info", "args"]
+helpstring = "whois <nick>"
+minlevel = 1
+
+def main(connection, info, args) :
+    if args[1] in connection.users["users"].keys() :
+        connection.ircsend(info["channel"], "%s has a user level of %s" % (args[1], connection.users["users"][args[1]]["userlevel"]))
+        if connection.users["users"][args[1]].has_key("channels") :
+            connection.ircsend(info["channel"], "%s is also on these channel's access lists: %s" % (args[1], ", ".join(connection.users["users"][args[1]]["channels"])))
