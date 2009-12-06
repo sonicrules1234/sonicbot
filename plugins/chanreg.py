7e7ce031448e8e0fa0313717bf5d57b8e3e48ebf
Westly Ward <westly@westly-laptop.(none)>
2009-12-05 19:37:45 -0800

    Fixed chanreg

--- plugins/chanreg.py
+++ plugins/chanreg.py
@@ -3,20 +3,20 @@ helpstring = "chanreg"
 minlevel = 2
 
 def main(connection, info, args) :
-    if auth(connection, info) :        
+    if auth(connection, info) : 
         if connection.users["users"][info["sender"]]["userlevel"] == 2 :
             connection.users["users"][info["sender"]]["userlevel"] = 3
             connection.users.sync()
         if not connection.users["users"][info["sender"]].has_key("channels") :
             connection.users["users"][info["sender"]]["channels"] = []
             connection.users.sync()
-            if info["channel"] not in connection.users["users"][info["sender"]]["channels"] :
-                connection.users["users"][info["sender"]]["channels"].append(info["channel"])
-                connection.users.sync()
-                connection.users["channels"][info["channel"]]["registered"] = True
-                connection.users.sync()
-                connection.ircsend(info["channel"], "%s: You have just registered %s" % (info["sender"], info["channel"]))
-            else : connection.ircsend(info["channel"], "%s: You are already on this channel's sonicbot access list!" % (info["sender"]))
+        if info["channel"] not in connection.users["users"][info["sender"]]["channels"] :
+            connection.users["users"][info["sender"]]["channels"].append(info["channel"])
+            connection.users.sync()
+            connection.users["channels"][info["channel"]]["registered"] = True
+            connection.users.sync()
+            connection.ircsend(info["channel"], "%s: You have just registered %s" % (info["sender"], info["channel"]))
+        else : connection.ircsend(info["channel"], "%s: You are already on this channel's sonicbot access list!" % (info["sender"]))
     else : connection.ircsend(info["channel"], "%s: You do not have at least half-ops on this channel.  If this is an error, please kick me and invite me again." % (info["sender"]))
 
 def auth(connection, info) :
