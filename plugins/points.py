c7cbc234140c1fe75ea03a4b1c5479d0660647a8
Westly Ward <westly@westly-laptop.(none)>
2009-12-07 11:29:46 -0800

    Added features to ignore users, limit number of point givings per day, and comparisons to points.py

--- plugins/points.py
+++ plugins/points.py
@@ -2,13 +2,16 @@ import time, shelve
 arguments = ["self", "info", "args"]
 minlevel = 1
 helpstring = "points [nick] [+/-number] or points [+/-]list or points -flush"
-
+maxperday = 2
 def main(connection, info, args) :
     points = shelve.open("points.db", writeback=True)
     if not points.has_key("users") :
         points["users"] = {}
         points.sync()
         points["userlist"] = []
+        points.sync()
+    if not points.has_key("ignored") :
+        points["ignored"] = {"nicks":[], "hostnames":[]}
         points.sync()
     if info["sender"].lower() not in points["users"].keys() :
         points["users"][info["sender"].lower()] = {"points":0, "outward":{}}
@@ -25,7 +28,7 @@ def main(connection, info, args) :
             if len(args) == 3 :
                 if args[1].lower() not in points["users"][info["sender"].lower()]["outward"] :
                     points["users"][info["sender"].lower()]["outward"][args[1].lower()] = 0
-                if args[1].lower() != info["sender"].lower() and points["users"][info["sender"].lower()]["outward"][args[1].lower()] < time.time() :
+                if args[1].lower() != info["sender"].lower() and points["users"][info["sender"].lower()]["outward"][args[1].lower()] < time.time() and verify(info["sender"].lower(), points) and info["sender"].lower() not in points["ignored"]["nicks"] and info["hostname"] not in points["ignored"]["hostnames"]:
                     sendpoints = int(args[2])
                     if sendpoints != 0 :
                         if sendpoints < -2 :
@@ -62,14 +65,47 @@ def main(connection, info, args) :
                 if args[1].startswith("+") : 
                     pointslist.reverse()
                     userlist.reverse()
-                pointslist = pointslist[:5]
+                pointslist = pointslist[:int(args[2])]
                 finalistlist = userlist
                 winnerlist = []
                 for point in pointslist :
                     winner = [x for x in finalistlist if points["users"][x]["points"] == point and x not in winnerlist][0]
                     winnerlist.append(winner)
                 connection.ircsend(info["channel"], "\n".join(["%s %s with %s point(s)" % (str(winnerlist.index(winner) + 1), winner, points["users"][winner]["points"]) for winner in winnerlist]))
-                
+            elif args[1].endswith("compare") :
+                if args[2].lower() not in points["users"].keys() :
+                    points["users"][args[2].lower()] = {"points":0, "outward":{}}
+                    points.sync()
+                if args[3].lower() not in points["users"].keys() :
+                    points["users"][args[3].lower()] = {"points":0, "outward":{}}
+                    points.sync()
+                connection.ircsend(info["channel"], "%s has % point(s) while %s has %s point(s).  This means %s" (args[2].lower(), str(points["users"][args[2].lower()]["points"]), args[3].lower(), str(points["users"][args[3].lower()]["points"]), compare(points, args[2].lower(), args[3].lower())))
+            elif args[1].endswith("ignore") :
+                if args[2].lower() not in points["ignored"]["nicks"] :
+                    points["ignored"]["nicks"].append(args[2].lower())
+                    points.sync()
+                if args[2] in connection.nicks.keys() :
+                    if connection.nicks[args[2]] not in points["ignored"]["hostnames"] :
+                        points["ignored"]["hostnames"].append(connection.nicks[args[2]])
+                        points.sync()
+                connection.ircsend(info["channel"], "%s will now be ignored" % (args[2]))
     else :
         connection.ircsend(info["channel"], "%s: You have %s point(s)." % (info["sender"], str(points["users"][info["sender"].lower()]["points"])))
     points.close()
+
+def verify(sender) :
+    history = [points["users"][sender]["outward"][x] for x in points["users"][sender]["outward"].keys()]
+    history.sort()
+    if len(history) in [0, 1] :
+        return True
+    elif time.time() >  history[-2] : return True
+    else : return False
+
+def compare(points, user1, user2) :
+    if points["users"][user1]["points"] == points["users"][user2]["points"] :
+        return "it is a tie"
+    elif points["users"][user1]["points"] > points["users"][user2]["points"] :
+        return "%s has %s more points over %s" % (user1, user2, str(points["users"][user1]["points"] - points["users"][user2]["points"]))
+    elif points["users"][user1]["points"] > points["users"][user2]["points"] :
+        return "%s has %s more points over %s" % (user2, user1, str(points["users"][user2]["points"] - points["users"][user1]["points"]))
+    
