18f1ade7863bb202ea6e6bfcad3a1f23b3478adf
Westly Ward <westly@westly-laptop.(none)>
2009-12-08 10:03:32 -0800

    Fixing rcheck so args[1] turns into an int

--- plugins/rcheck.py
+++ plugins/rcheck.py
@@ -5,5 +5,5 @@ minlevel = 4
 
 def main(connection, info, args, world) :
     reports = shelve.open("reports-%s.db" % (world.hostnicks[connection.host]), writeback=True)
-    connection.ircsend(info["channel"], "%s reported %s in %s with the reason '%s' and a priority level of %s on %s" % (reports["reports"][args[1]]["reporter"], reports["reports"][args[1]]["accused"], reports["reports"][args[1]]["channel"], reports["reports"][args[1]]["reason"], reports["reports"][args[1]]["priority"], time.strftime("%b %d %Y, %H:%M:%S %Z", time.gmtime(reports["reports"][args[1]]["time"]))))
+    connection.ircsend(info["channel"], "%s reported %s in %s with the reason '%s' and a priority level of %s on %s" % (reports["reports"][int(args[1])]["reporter"], reports["reports"][int(args[1])]["accused"], reports["reports"][int(args[1])]["channel"], reports["reports"][int(args[1])]["reason"], reports["reports"][int(args[1])]["priority"], time.strftime("%b %d %Y, %H:%M:%S %Z", time.gmtime(reports["reports"][int(args[1])]["time"]))))
     reports.close()
