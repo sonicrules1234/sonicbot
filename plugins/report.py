16a630a2cf89a1944e1f5b71f39f46a1f92d6f2a
Westly Ward <westly@westly-laptop.(none)>
2009-12-08 09:56:46 -0800

    Fixing report.py

--- plugins/report.py
+++ plugins/report.py
@@ -8,7 +8,7 @@ def main(connection, info, args, world, conf) :
     if not reports.has_key("reports") :
         reports["reports"] = []
         reports.sync()
-    reports.append({"priority":args[3], "time":time.time(), "reporter":[info["sender"].lower(), connection.nicks[info["sender"]]], "channel":args[2], "accused":args[1].lower(), "reason":" ".join(args[4:]), "password":"".join([random.choice(string.ascii_lowercase+string.ascii_uppercase) for x in range(6)]), "id":len(reports["reports"]), "status":"reported", "assigned":[], "response":{}})
+    reports["reports"].append({"priority":args[3], "time":time.time(), "reporter":[info["sender"].lower(), connection.nicks[info["sender"]]], "channel":args[2], "accused":args[1].lower(), "reason":" ".join(args[4:]), "password":"".join([random.choice(string.ascii_lowercase+string.ascii_uppercase) for x in range(6)]), "id":len(reports["reports"]), "status":"reported", "assigned":[], "response":{}})
     reports.sync()
     connection.ircsend(conf.staffchannel[connection.host], "%s has reported %s in %s with the reason '%s' and a priority level of %s.  This report's ID is %s." % (reports["reports"][-1]["reporter"], reports["reports"][-1]["accused"], reports["reports"][-1]["channel"], reports["reports"][-1]["reason"], reports["reports"][-1]["priority"], reports["reports"][-1]["id"]))
     reports.close()
