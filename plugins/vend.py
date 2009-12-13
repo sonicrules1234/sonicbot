4e79bdcefd8beb30b05f804085cae801ef797e30
Westly Ward <westly@westly-laptop.(none)>
2009-12-13 14:37:08 -0800

    Fixing vend.py

--- plugins/vend.py
+++ plugins/vend.py
@@ -7,11 +7,13 @@ def main(connection, info, args, world) :
     votes = shelve.open("votes.db", writeback=True)
     if votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["started"] :
         connection.ircsend(info["channel"], "The vote on '%s' has ended.  Here are the stats:" % (votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["topic"]))
-        connection.ircsend(info["channel"], "\n".join(["Choice %s: '%s' got %s votes" % (str(x + 1), votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["choice"], str(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["voters"]))) for x in range(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"]))]))
+        connection.ircsend(info["channel"], "\n".join(["Choice %s: '%s' got %s votes" % (str(x + 1), votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["choice"], str(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["votes"]))) for x in range(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"]))]))
         scores = [len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["voters"]) for x in range(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"]))]
         winningscore = max(scores)
         winners = [x["choice"] for x in votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"] if len(x["votes"]) == winningscore]
         if len(winner) == 1 : connection.ircsend(info["channel"], "The winner is %s!" % (winners[0]))
         else : connection.ircsend(info["channels"], "The winners with a tie in 1st place are: %s and %s." % (", ".join(winners[:-1]), winners[-1]))
+        votes["networks"][world.hostnicks[connection.host]].pop(int(args[1]) - 1)
+        votes.sync()
     else : connection.ircsend(info["channel"], "That vote has not yet started!")
     votes.close()
