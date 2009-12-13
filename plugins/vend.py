6a7eeb6e7ca2cba920cf6d75f616544850758026
Westly Ward <westly@westly-laptop.(none)>
2009-12-13 14:39:43 -0800

    Fixing vend.py to display the winner of a vote

--- plugins/vend.py
+++ plugins/vend.py
@@ -8,7 +8,7 @@ def main(connection, info, args, world) :
     if votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["started"] :
         connection.ircsend(info["channel"], "The vote on '%s' has ended.  Here are the stats:" % (votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["topic"]))
         connection.ircsend(info["channel"], "\n".join(["Choice %s: '%s' got %s votes" % (str(x + 1), votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["choice"], str(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["votes"]))) for x in range(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"]))]))
-        scores = [len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["voters"]) for x in range(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"]))]
+        scores = [len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"][x]["votes"]) for x in range(len(votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"]))]
         winningscore = max(scores)
         winners = [x["choice"] for x in votes["networks"][world.hostnicks[connection.host]][int(args[1]) - 1]["choices"] if len(x["votes"]) == winningscore]
         if len(winner) == 1 : connection.ircsend(info["channel"], "The winner is %s!" % (winners[0]))
