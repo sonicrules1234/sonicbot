42a4c1fc45dc708cd05cf403b1e7f99e75cc2d9b
Westly Ward <westly@westly-laptop.(none)>
2009-12-25 12:33:56 -0800

    Fixing a bug having to do with brackets in nicks for some of the mail plugins

--- plugins/mail.py
+++ plugins/mail.py
@@ -5,29 +5,29 @@ minlevel = 1
 
 def main(connection, info, args) :
     mail = shelve.open("mail.db", writeback=True)
-    if not mail.has_key(args[1]) :
+    if not mail.has_key(args[1].replace("[", "").replace("]", "")) :
         connection.ircsend(info["channel"], "Sorry, no such user %s in my database." % (args[1]))
     else :
-        if not mail[args[1]]["messages"].has_key(info["sender"]) :
-            mail[args[1]]["messages"][info["sender"]] = {}
-            mail[args[1]]["userorder"].append(info["sender"])
+        if not mail[args[1]]["messages"].has_key(info["sender"].replace("[", "").replace("]", "")) :
+            mail[args[1].replace("[", "").replace("]", "")]["messages"][info["sender"].replace("[", "").replace("]", "")] = {}
+            mail[args[1].replace("[", "").replace("]", "")]["userorder"].append(info["sender"].replace("[", "").replace("]", ""))
             mail.sync()
-            mail[args[1]]["messages"][info["sender"]]["msgorder"] = []
+            mail[args[1].replace("[", "").replace("]", "")]["messages"][info["sender"].replace("[", "").replace("]", "")]["msgorder"] = []
             mail.sync()
-        if len(mail[args[1]]["messages"][info["sender"]]["msgorder"]) < 10 :
+        if len(mail[args[1].replace("[", "").replace("]", "")]["messages"][info["sender"].replace("[", "").replace("]", "")]["msgorder"]) < 10 :
             timet = str(int(time.time()))
             nogo = False 
-            for message in mail[args[1]]["messages"][info["sender"]]["msgorder"] :
+            for message in mail[args[1]]["messages"][info["sender"].replace("[", "").replace("]", "")]["msgorder"] :
                 if message[0] == timet :
                     nogo =  True
                     break
             if not nogo :
-                mail[args[1]]["messages"][info["sender"]][timet] = " ".join(args[2:])
+                mail[args[1].replace("[", "").replace("]", "")]["messages"][info["sender"].replace("[", "").replace("]", "")][timet] = " ".join(args[2:])
                 mail.sync()
-                mail[args[1]]["messages"][info["sender"]]["msgorder"].append([timet, True])
+                mail[args[1].replace("[", "").replace("]", "")]["messages"][info["sender"].replace("[", "").replace("]", "")]["msgorder"].append([timet, True])
                 mail.sync()
-                connection.ircsend(info["channel"], "%s: Your message was successfully sent." % (info["sender"]))
-                mail[args[1]]["notify"] = True
+                connection.ircsend(info["channel"], "%s: Your message was successfully sent." % (info["sender"].replace("[", "").replace("]", "")))
+                mail[args[1].replace("[", "").replace("]", "")]["notify"] = True
                 mail.sync()
-        else : connection.ircsend(info["channel"], "%s: You may only send 10 messages at a time.  You may send more once %s has checked some of your others." % (info["sender"], args[1]))
+        else : connection.ircsend(info["channel"], "%s: You may only send 10 messages at a time.  You may send more once %s has checked some of your others." % (info["sender"].replace("[", "").replace("]", ""), args[1].replace("[", "").replace("]", "")))
     mail.close()
--- plugins/mailman.py
+++ plugins/mailman.py
@@ -6,36 +6,36 @@ helpstring = "mailman <command> <message/nick>"
 def main(connection, info, args, conf) :
     mail = shelve.open("mail.db", writeback=True)
     if args[1] == "delete" :
-        if mail.has_key(args[2]) :
-            del mail[args[2]]
+        if mail.has_key(args[2].replace("[", "").replace("]", "")) :
+            del mail[args[2].replace("[", "").replace("]", "")]
             mail.sync()
             connection.ircsend(info["channel"], "User successfully deleted")
         else : connection.ircsend(info["channel"], "No such user")
     elif args[1] == "global" :
         timet = str(int(time.time()))
         for user in mail.keys() :
-            if not mail[user]["messages"].has_key(info["sender"]) :
-                mail[user]["messages"][info["sender"]] = {}
-                mail[user]["userorder"].append(info["sender"])
+            if not mail[user]["messages"].has_key(info["sender"].replace("[", "").replace("]", "")) :
+                mail[user]["messages"][info["sender"].replace("[", "").replace("]", "")] = {}
+                mail[user]["userorder"].append(info["sender"].replace("[", "").replace("]", ""))
                 mail.sync()
-                mail[user]["messages"][info["sender"]]["msgorder"] = []
+                mail[user]["messages"][info["sender"].replace("[", "").replace("]", "")]["msgorder"] = []
                 mail.sync()
-            mail[user]["messages"][info["sender"]][timet] = " ".join(args[2:])
-            mail[user]["messages"][info["sender"]]["msgorder"].append([timet, True])
+            mail[user]["messages"][info["sender"].replace("[", "").replace("]", "")][timet] = " ".join(args[2:])
+            mail[user]["messages"][info["sender"].replace("[", "").replace("]", "")]["msgorder"].append([timet, True])
             mail[user]["notify"] = True
             mail.sync()
         mail.sync()
-        connection.ircsend(info["channel"], "%s: Your global message has been sent." % (info["sender"]))
+        connection.ircsend(info["channel"], "%s: Your global message has been sent." % (info["sender"].replace("[", "").replace("]", "")))
     elif args[1] == "passreset" :
         newpass = ""
         for x in range(6) :
             newpass += random.choice(string.letters)
         connection.ircsend(args[2], "Your password has been reset to '%s'.  Please visit %s and login using that password.  Once in, you can change it to whatever you want." % (newpass, conf.mail_url))
-        mail[args[2]]["password"] = hashlib.sha512(newpass).hexdigest()
+        mail[args[2].replace("[", "").replace("]", "")]["password"] = hashlib.sha512(newpass).hexdigest()
         mail.sync()
         connection.ircsend(info["channel"], "Password reset successfully.")
     elif args[1] == "newhost" :
-        mail[args[2]]["hostname"].append(args[3])
+        mail[args[2].replace("[", "").replace("]", "")]["hostname"].append(args[3])
         mail.sync()
         connection.ircsend(info["channel"], "Hostname changed successfully")
     elif args[1] == "autohost" :
