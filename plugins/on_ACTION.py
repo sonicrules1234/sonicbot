5e1f306266763420686e56d380510765f511136f
Westly Ward <westly@westly-laptop.(none)>
2009-12-25 10:17:08 -0800

    Added some automatic responses to when somebody hurts sonicbot in an action

--- plugins/on_ACTION.py
+++ plugins/on_ACTION.py
@@ -1,6 +1,6 @@
-import shelve, time
+import shelve, time, random
 def main(connection, info, conf) :
-    args = info["message"].split(" ")[1:]
+    args = info["message"].split(" ")
     contextdb = shelve.open("context.db", writeback=True)
     if not contextdb.has_key(info["channel"]) and info["channel"].startswith("#") :
         contextdb[info["channel"]] = ["<%s> %s" % (info["sender"], info["message"])]
@@ -18,3 +18,7 @@ def main(connection, info, conf) :
         seendb.sync()
     seendb["users"][info["sender"].lower()] = [time.time(), "*%s %s" % (info["sender"], " ".join(args[1:]).replace("", ""))]
     seendb.close()
+    if args[1] in ["slaps", "punches", "stomps", "hurts", "hits", "fucks", "smacks", "crunches", "kicks", "barfs", "forces", "force", "squishes", "bodyslams", "shoots", "compresses"] :
+        if args[2] == "sonicbot" or args[-1] == "sonicbot" :
+            connection.ircsend(info["channel"], random.choice(["Oww!", "Ouch, that hurt!", "\x01ACTION curls up in fetal position\x01", "\x01ACTION slaps %s\x01" % (info["sender"]), "\x01ACTION smacks %s\x01" % (info["sender"])]))
+            
