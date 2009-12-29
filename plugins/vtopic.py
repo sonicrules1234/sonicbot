717f3e35f7417d490a1e95c48b0728eca11b65f3
Westly Ward <westly@westly-laptop.(none)>
2009-12-28 20:51:19 -0800

    Fixing vtopic.py

--- plugins/vtopic.py
+++ plugins/vtopic.py
@@ -8,6 +8,9 @@ def main(connection, info, args, world) :
     if not votes.has_key("networks") :
         votes["networks"] = {world.hostnicks[connection.host]:[]}
         votes.sync()
+    if not votes["networks"].has_key(world.hostnicks[connection.host]) :
+        votes["networks"][world.hostnicks[connection.host]] = []
+        votes.sync()
     votes["networks"][world.hostnicks[connection.host]].append({"topic":" ".join(args[1:]), "choices":[], "started":False, "voters":[]})
     votes.sync()
     connection.ircsend(info["channel"], "The topic '%s' has been given the topic id of %s" % (" ".join(args[1:]), len(votes["networks"][world.hostnicks[connection.host]])))
