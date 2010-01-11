ac27723601fafc5e3383b3a58ee08a595744cdcd
Westly Ward <westly@westly-laptop.(none)>
2010-01-11 07:02:47 -0800

    Merging the mcheck command and mail command together, also deleteing the restart command

--- plugins/restart.py
+++
@@ -1,7 +0,0 @@
-import os
-arguments = ["self", "info", "args"]
-minlevel = 5
-helpstring = "restart"
-def main(connection, info, args) :
-    os.popen2('echo "python sonicbot-v3.py" | at now + 1 minutes')
-    connection.rawsend("QUIT :Leaving...\n")
