43ab18ffcdde0bee62eb0bd093f3cde068172a57
Westly Ward <westly@wardw.ath.cx>
2010-01-18 17:28:39 -0800

    Making the base64 plugin address the sender so that it cannot be used to trigger other bots

--- plugins/b64.py
+++ plugins/b64.py
@@ -5,6 +5,6 @@ arguments = ["self", "info", "args"]
 
 def main(connection, info, args) :
     if args[1] == "encode" :
-        connection.ircsend(info["channel"], base64.b64encode(" ".join(args[2:])))
+        connection.ircsend(info["channel"], "%s: %s" % (info["sender"], base64.b64encode(" ".join(args[2:]))))
     elif args[1] == "decode" :
-        connection.ircsend(info["channel"], base64.b64decode(args[2]))
+        connection.ircsend(info["channel"], "%s: %s" % (info["sender"], base64.b64decode(args[2])))
