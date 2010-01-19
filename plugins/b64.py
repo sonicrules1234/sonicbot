d8b0317d1824a03f78171a111a30b4eedf9bf35c
Westly Ward <westly@wardw.ath.cx>
2010-01-18 17:16:32 -0800

    Adding a base64 plugin

---
+++ plugins/b64.py
@@ -0,0 +1,10 @@
+import base64
+helpstring = "b64 encode|decode <string>"
+minlevel = 1
+arguments = ["self", "info", "args"]
+
+def main(connection, info, args) :
+    if args[1] == "encode" :
+        connection.ircsend(info["channel"], base64.b64encode(" ".join(args[2:])))
+    elif args[1] == "decode" :
+        connection.ircsend(info["channel"], base64.b64decode(args[2]))
