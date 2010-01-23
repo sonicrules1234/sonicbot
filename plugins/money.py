537e8992da70af9557679c53f74e6fa2d6825eec
Westly Ward <westly@wardw.ath.cx>
2010-01-23 13:37:24 -0800

    Fixing the money command so you can specify a target

--- plugins/money.py
+++ plugins/money.py
@@ -7,6 +7,7 @@ def main(connection, info, args, world) :
     money = shelve.open("money-%s.db" % (world.hostnicks[connection.host]), writeback=True)
     if len(args) == 1 :
         target = info["sender"]
+    else : target = args[1]
     if money.has_key(target) :
         connection.ircsend(info["channel"], "%s has %s dollars." % (target, str(money[target]["money"])))
     else : connection.ircsend(info["channel"], "No such user %s in my database." % (target))
