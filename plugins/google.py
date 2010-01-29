192bb79365b14b0ebdc1cab954efc52455b383bf
Westly Ward <westly@wardw.ath.cx>
2010-01-29 11:54:36 -0800

    Fixing a syntax error

--- plugins/google.py
+++ plugins/google.py
@@ -11,4 +11,4 @@ def main(connection, info, args) :
         source = f.read()
         resultlist = re.findall(r'<h3 class=r><a href="(http://.+?)"', source)
         resultlist2 = re.findall(r'<div class="s">(.+?)<br>', source)
-        connection.ircsend(info["channel"], "%s - %s" % (resultlist[0], resultlist2[0].replace("<em>", "\x02").replace("</em>", "\x02").replace("<b>", "\x02").replace("</b>", "\x02"))
+        connection.ircsend(info["channel"], "%s - %s" % (resultlist[0], resultlist2[0].replace("<em>", "\x02").replace("</em>", "\x02").replace("<b>", "\x02").replace("</b>", "\x02")))
