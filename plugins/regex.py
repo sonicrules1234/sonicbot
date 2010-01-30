a1a04d2c3d52d1f89d879b3b0a353161612670d8
Westly Ward <westly@wardw.ath.cx>
2010-01-30 14:03:02 -0800

    Fixing the regex plugin up a bit

--- plugins/regex.py
+++ plugins/regex.py
@@ -4,4 +4,4 @@ helpstring = "regex <pattern> <string>"
 minlevel = 1
 
 def main(connection, info, args) :
-    connection.ircsend(info["channel"], repr(re.search(args[1], args[2])))
+    connection.ircsend(info["channel"], repr(re.search(args[1], args[2]).groups()))
