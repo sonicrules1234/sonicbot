9f27d628adcc624ade863d10a9095628be4ed1b1
Westly Ward <westly@wardw.ath.cx>
2010-02-12 09:05:04 -0800

    Making it so that sonicbot notifies the owner when joining a channel after invite

--- plugins/on_INVITE.py
+++ plugins/on_INVITE.py
@@ -1,3 +1,4 @@
 helpstring = "This is an event plugin"
 def main(connection, info, conf) :
     connection.rawsend("JOIN %s\n"%info['words'][3][1:])
+    connection.ircsend(conf.owner, "Joining %s after being invited..." % (info['words'][3][1:]))
