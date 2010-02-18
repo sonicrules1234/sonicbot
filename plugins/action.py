95c4ee97304720565033d004fa0da6ff846d46e8
Westly Ward <westly@wardw.ath.cx>
2010-02-18 14:54:25 -0800

    Fixing a typo with docstrings

--- plugins/action.py
+++ plugins/action.py
@@ -2,5 +2,5 @@ arguments = ["self", "info", "args"]
 minlevel = 4
 helpstring = "action <channel> <action>"
 def main(connection, info, args) :
-    """Does an action, ie /me"
+    """Does an action, ie /me"""
     connection.ircsend(args[1], "\x01ACTION %s\x01" % (" ".join(args[2:])))
