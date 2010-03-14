d91eb7861b8050e1a1d6f4e4cdc4ef69d01ce16c
Westly Ward <westly@westly-laptop.(none)>
2010-03-14 14:25:34 -0700

    Fixing sonicbot for a workaround so that it does not send \r\r, because unreal counts it as a newline for some reason

--- plugins/title.py
+++ plugins/title.py
@@ -6,5 +6,5 @@ helpstring = "title <url>"
 def main(connection, info, args) :
     """Gets the title of a url"""
     link = urllib2.urlopen(args[1])
-    title = re.search(r"\<title\>(.+?)\</title\>", link.read(2000).replace("\n", "").replace("\t", "")).group(1)
+    title = re.search(r"\<title\>(.+?)\</title\>", link.read(2000).replace("\n", "").replace("\t", "").replace("\r", "")).group(1) #\r being stripped is because of an UnrealIRCd bug
     connection.ircsend(info["channel"], '%s: "%s"' % (info["sender"], title))
