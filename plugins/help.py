454d0ec93a77637ca67691f883288f1f4ddbfafe
Westly Ward <westly@westly-laptop.(none)>
2010-03-29 08:11:02 -0700

    Making sonicbot notice the command list in ;help

--- plugins/help.py
+++ plugins/help.py
@@ -4,13 +4,14 @@ minlevel = 1
 def main(connection, plugs, args, info, conf) :
     """Returns a help message"""
     if " " not in info["message"] :
-        connection.ircsend(info["channel"], "This bot's prefix is %s  The following is a list of available commands:" % (conf.prefix))
+        connection.ircsend(info["channel"], "%s: I will notice you a list of my commands, because it is too big to put here.")
+        connection.ircsend(info["sender"], "This bot's prefix is %s  The following is a list of available commands:" % (conf.prefix))
         pluglist = []
         for plug in plugs["pluginlist"].pluginlist :
             if plug in connection.users["channels"][info["channel"]]["enabled"] :
                 pluglist.append(plug)
-        connection.ircsend(info["channel"], ", ".join(pluglist))
-        connection.ircsend(info["channel"], "The help for this module is help <plugin>")
+        connection.ircsend(info["sender"], ", ".join(pluglist))
+        connection.ircsend(info["sender"], "The help for this module is help <plugin>")
     else :
         if args[1] in plugs['pluginlist'].pluginlist :
             try :
