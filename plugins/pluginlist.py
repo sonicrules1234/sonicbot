7249d21f87295b2aa56d1bd1f5f42c768fb022af
Westly Ward <westly@westly-laptop.(none)>
2010-05-15 11:17:07 -0700

    The plugin list is now alphabatized

--- plugins/pluginlist.py
+++ plugins/pluginlist.py
@@ -10,8 +10,8 @@ for plugin in glob.glob("plugins/*.py") :
 for plugin in glob.glob("plugins/*.py") :
     if plugin != "plugins/__init__.py" and plugin != "plugins\\__init__.py" and plugin.replace("plugins\\", "").replace("plugins/", "").replace(".py", "").startswith("on_"):
         eventlist.append(plugin.replace("plugins\\", "").replace("plugins/", "").replace(".py", ""))
-
+pluginlist.sort()
 
 def main(connection, info, args) :
     """Lists available plugins"""
-    connection.ircsend(info["channel"], "The available plugins are %s" % (", ".join([x for x in pluginlist if x in connection.users["channels"][info["channel"]]["enabled"]])))
+    connection.ircsend(info["channel"], _("The available plugins are %(listofplugins)s") % dict(listofplugins=", ".join([x for x in pluginlist if x in connection.users["channels"][info["channel"]]["enabled"]])))
