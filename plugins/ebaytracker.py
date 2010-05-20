bea9ec7c88b1b409c2d7c45b81affb5dbf689fec
Westly Ward <sonicrules1234@gmail.com>
2010-05-19 18:05:36 -0700

    Fixing ebaytracker

--- plugins/ebaytracker.py
+++ plugins/ebaytracker.py
@@ -17,28 +17,28 @@ def main(connection, info, args, conf, world, thread) :
             feeds[feedurl]["updated"] = 0
             feeds.sync()
             feeds.close()
-            if not world.feeds.has_key(connection.host) :
+            if not world.feeds.haskey(connection.host) :
                 world.feeds[connection.host] = {}
-            if not world.feeds[connection.host].has_key(info["channel"]) :
+            if not world.feeds[connection.host].haskey(info["channel"]) :
                 world.feeds[connection.host][info["channel"]] = {}
-            if not world.feeds[connection.host][info["channel"]].has_key(feedurl) :
+            if not world.feeds[connection.host][info["channel"]].haskey(feedurl) :
                 world.feeds[connection.host][info["channel"]][feedurl] = []
             if len(world.feeds[connection.host][info["channel"]][feedurl]) == 0 :
                 world.feeds[connection.host][info["channel"]][feedurl].append(True)
                 indexnum = len(world.feeds[connection.host][info["channel"]][feedurl]) - 1
-                thread.start_new_thread(get_feed, (connection, info, args, feedurl, world, indexnum, title))
+                thread.startnewthread(getfeed, (connection, info, args, feedurl, world, indexnum, title))
             elif not world.feeds[connection.host][info["channel"]][feedurl][-1] :
                 world.feeds[connection.host][info["channel"]][feedurl].append(True)
                 indexnum = len(world.feeds[connection.host][info["channel"]][feedurl]) - 1
-                thread.start_new_thread(get_feed, (connection, info, args, feedurl, world, indexnum, title))
+                thread.startnewthread(getfeed, (connection, info, args, feedurl, world, indexnum, title))
             else : connection.ircsend(info["channel"], "That item is already being tracked.")
         elif onoff == "off" :
             if feedurl in world.feeds[connection.host][info["channel"]].keys() :
                     world.feeds[connection.host][info["channel"]][feedurl][-1] = False
-                    connection.ircsend(info["channel"], _("Stopped tracking item at %(url)s") % dict(url=feedurl))
-            else : connection.ircsend(info["channel"], _("No such item being tracked."))
+                    connection.ircsend(info["channel"], ("Stopped tracking item at %(url)s") % dict(url=feedurl))
+            else : connection.ircsend(info["channel"], ("No such item being tracked."))
     else : connection.ircsend(info["channel"], "That is not a valid ebay url")
-def get_feed(connection, info, args, feedurl, world, indexnum, title) :
+def getfeed(connection, info, args, feedurl, world, indexnum, title) :
     """Checks for a change in the price on the page"""
     while world.feeds[connection.host][info["channel"]][feedurl][indexnum] :
         feeds = shelve.open("feeds-%s.db" % (world.hostnicks[connection.host]), writeback=True)
@@ -50,10 +50,10 @@ def get_feed(connection, info, args, feedurl, world, indexnum, title) :
         if price != feeds[feedurl]["updated"] and feeds[feedurl]["updated"] != 0 :
             feeds[feedurl]["updated"] = price
             feeds.sync()
-            connection.ircsend(info["channel"], _("Another bid on %(itemtitle)s has been made!  The price is now %(newprice)s") % dict(itemtitle=title, newprice=price))
+            connection.ircsend(info["channel"], ("Another bid on %(itemtitle)s has been made!  The price is now %(newprice)s") % dict(itemtitle=title, newprice=price))
         elif feeds[feedurl]["updated"] == 0 :
             feeds[feedurl]["updated"] = price
             feeds.sync()
-            connection.ircsend(info["channel"], _("Started tracking '%(itemtitle)s' with price of %(newprice)s.") % dict(itemtitle=title, newprice=price))
+            connection.ircsend(info["channel"], ("Started tracking '%(itemtitle)s' with price of %(newprice)s.") % dict(itemtitle=title, newprice=price))
         feeds.close()
         time.sleep(120)
