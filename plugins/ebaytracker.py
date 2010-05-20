551f6fcfe68f105d001984f5e1f3a26c8ee535ba
Westly Ward <sonicrules1234@gmail.com>
2010-05-19 18:02:22 -0700

    Fixing ebaytracker

--- plugins/ebaytracker.py
+++ plugins/ebaytracker.py
@@ -35,25 +35,25 @@ def main(connection, info, args, conf, world, thread) :
         elif onoff == "off" :
             if feedurl in world.feeds[connection.host][info["channel"]].keys() :
                     world.feeds[connection.host][info["channel"]][feedurl][-1] = False
-                    connection.ircsend(info["channel"], "Stopped tracking item at %s" % (feedurl))
-            else : connection.ircsend(info["channel"], "No such item being tracked.")
+                    connection.ircsend(info["channel"], _("Stopped tracking item at %(url)s") % dict(url=feedurl))
+            else : connection.ircsend(info["channel"], _("No such item being tracked."))
     else : connection.ircsend(info["channel"], "That is not a valid ebay url")
 def get_feed(connection, info, args, feedurl, world, indexnum, title) :
     """Checks for a change in the price on the page"""
     while world.feeds[connection.host][info["channel"]][feedurl][indexnum] :
         feeds = shelve.open("feeds-%s.db" % (world.hostnicks[connection.host]), writeback=True)
         good = urllib.urlopen(feedurl).read()
-        price = re.search(r'<span id\=\"v4\-11\" class\=\"vi\-is1\-prcp\">(.+?)<\/span>', good).group(1)
+        price = re.search(r'\<td class\=\"vi\-is1\-solid vi\-is1\-tbll\"\>\<span\>\<span.*class\=\"vi\-is1\-prcp\".*\>(.*)\<\/span\>\<\/span\>\<\/td\>\<td colspan\=\"2\" class\=\"vi\-is1\-solid vi\-is1\-tblb\"\>', good).group(1)
 
         print "Checking feed"
         print "Recorded = %s ; Feed = %s" % (feeds[feedurl]["updated"], price)
         if price != feeds[feedurl]["updated"] and feeds[feedurl]["updated"] != 0 :
             feeds[feedurl]["updated"] = price
             feeds.sync()
-            connection.ircsend(info["channel"], "Another bid on %s has been made!  The price is now %s" % (title, price))
+            connection.ircsend(info["channel"], _("Another bid on %(itemtitle)s has been made!  The price is now %(newprice)s") % dict(itemtitle=title, newprice=price))
         elif feeds[feedurl]["updated"] == 0 :
             feeds[feedurl]["updated"] = price
             feeds.sync()
-            connection.ircsend(info["channel"], "Started tracking '%s' with price of %s." % (title, price))
+            connection.ircsend(info["channel"], _("Started tracking '%(itemtitle)s' with price of %(newprice)s.") % dict(itemtitle=title, newprice=price))
         feeds.close()
         time.sleep(120)
