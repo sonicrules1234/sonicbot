a2505e7682b40446eff953032f93862e19976786
Westly Ward <sonicrules1234@gmail.com>
2010-05-21 07:17:46 -0700

    Fixing a bug in geoip

--- plugins/geoip.py
+++ plugins/geoip.py
@@ -5,4 +5,5 @@ helpstring = "geoip <IP or hostname>"
 minlevel = 1
 
 def main(connection, info, args) :
-    connection.ircsend(info["channel"], "%s: %s" % (info["sender"], repr(pyipinfodb.GetCity(args[1]))))
+    if args[1] != "" : connection.ircsend(info["channel"], "%s: %s" % (info["sender"], repr(pyipinfodb.GetCity(args[1]))))
+    else : connection.ircsend(info["channel"], "You need to specify a host.")
