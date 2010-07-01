c6c0b0f23d4987b2b0b916467114cb103336a932
Westly Ward <sonicrules1234@gmail.com>
2010-06-30 19:05:07 -0700

    Fixing context for v4

--- sonicbotv4/oldplugins/context.py
+++ sonicbotv4/oldplugins/context.py
@@ -6,5 +6,5 @@ def main(connection, info, args) :
     """Returns the last 10 lines in the channel"""
     context = shelve.open("context.db")
     if context.has_key(info["channel"]) :
-        connection.msg(info["sender"], "\n".join(context[info["channel"]]))
+        connection.notice(info["sender"], "\n".join(context[info["channel"]]))
     context.close()
