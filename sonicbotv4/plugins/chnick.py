90ec09cbdc917da3d280d7d802f18aa4c30eb321
Westly Ward <sonicrules1234@gmail.com>
2010-07-02 08:22:56 -0700

    Adding docstrings to chnick

--- sonicbotv4/plugins/chnick.py
+++ sonicbotv4/plugins/chnick.py
@@ -1,4 +1,6 @@
 def startup(addPluginHook, addHook, world) :
     addPluginHook(world, "chnick", main, 1, ["self", "info", "args", "world"])
 def main(self, info, args, world) :
+    """chnick <new nick>
+Changes sonicbot's nick"""
     self.rawsend("NICK %s\r\n" % (args[1]))
