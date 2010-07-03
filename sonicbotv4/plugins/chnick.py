d75e46079f04eb19440eb4b6ea9d75b1de65060e
Westly Ward <sonicrules1234@gmail.com>
2010-07-02 21:21:20 -0700

    Fixing chnick to be admin only

--- sonicbotv4/plugins/chnick.py
+++ sonicbotv4/plugins/chnick.py
@@ -1,5 +1,5 @@
 def startup(addPluginHook, addHook, world) :
-    addPluginHook(world, "chnick", main, 1, ["self", "info", "args", "world"])
+    addPluginHook(world, "chnick", main, 4, ["self", "info", "args", "world"])
 def main(self, info, args, world) :
     """chnick <new nick>
 Changes sonicbot's nick"""
