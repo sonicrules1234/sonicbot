68b65ccbe99d55febdb312767ab1df1dac7e7d5d
Westly Ward <sonicrules1234@gmail.com>
2010-07-19 18:48:59 -0700

    Updating the coin plugin

--- oldplugins/coin.py
+++ oldplugins/coin.py
@@ -5,7 +5,7 @@ helpstring = "coin <bet>"
 
 def main(connection, info, args, world) :
     """Decides heads or tails based on the coinchance variable.   Adds or removes appropriate amount of money"""
-    money = shelve.open("money-%s.db" % (world.hostnicks[connection.host]), writeback=True)
+    money = shelve.open("money-%s.db" % (world.networkname), writeback=True)
     if money.has_key(info["sender"]) :
         bet = int(args[1])
         if bet <= money[info["sender"]]["money"] and bet >= 1 :
