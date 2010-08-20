f5b9c2be656e76385e3cdea997c8183f8c64917e
Westly Ward <sonicrules1234@gmail.com>
2010-08-20 06:22:00 -0700

    Fixing up the dehop plugin

--- oldplugins/dehop.py
+++ oldplugins/dehop.py
@@ -7,5 +7,5 @@ def main(connection, info, args, conf) :
     if len(args) == 1 : target = info["sender"]
     else : target = args[1]
     connection.rawsend("MODE %s -h %s\n" % (info["channel"], target))
-    if info["channel"] in conf.avchans :
-        connection.rawsend("MODE %s +v %s\n" % (info["channel"], target))
+#    if info["channel"] in conf.avchans :
+#        connection.rawsend("MODE %s +v %s\n" % (info["channel"], target))
