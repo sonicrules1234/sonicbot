45e930b46544270353584706c30912f8004e8910
Westly Ward <sonicrules1234@gmail.com>
2010-10-21 21:32:55 -0700

    Fixing pm.py

--- plugins/pm.py
+++ plugins/pm.py
@@ -1,4 +1,4 @@
-def startup(addPLuginHook, addHook, world) :
+def startup(addPluginHook, addHook, world) :
     addPluginHook(world, "pm", main, 5, ["self", "info", "args", "world"])
 def main(self, info, args, world) :
     self.pm(args[1], " ".join(args[2:]))
