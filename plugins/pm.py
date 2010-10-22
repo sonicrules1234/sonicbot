5181c1ecd2b139d89d06dbf57326d3405cff090d
Westly Ward <sonicrules1234@gmail.com>
2010-10-21 21:35:56 -0700

    Adding docs for pm.py

--- plugins/pm.py
+++ plugins/pm.py
@@ -1,4 +1,6 @@
 def startup(addPluginHook, addHook, world) :
     addPluginHook(world, "pm", main, 5, ["self", "info", "args", "world"])
 def main(self, info, args, world) :
+    """pm <nick> <message>
+Makes a private message"""
     self.pm(args[1], " ".join(args[2:]))
