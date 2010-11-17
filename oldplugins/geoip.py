2c0067f8b954820035f577a8c7e7b1fe5c882534
Westly Ward <sonicrules1234@gmail.com>
2010-11-17 11:18:31 -0800

    Fixing geoip

--- oldplugins/geoip.py
+++ oldplugins/geoip.py
@@ -1,9 +1,10 @@
 import imp
 pyipinfodb = imp.load_source("pyipinfodb", "pyipinfodb/pyipinfodb.py")
-arguments = ["self", "info", "args"]
+arguments = ["self", "info", "args", "world"]
 helpstring = "geoip <IP or hostname>"
 minlevel = 1
-IPInfo = pyipinfodb.IPInfo(world.globalsettings["IPInfoDB-apikey"])
-def main(connection, info, args) :
+
+def main(connection, info, args, world) :
+    IPInfo = pyipinfodb.IPInfo(world.globalsettings["IPInfoDB-apikey"])
     if args[1] != "" : connection.msg(info["channel"], "%s: %s" % (info["sender"], repr(IPInfo.GetCity(args[1]))))
     else : connection.msg(info["channel"], "You need to specify a host.")
