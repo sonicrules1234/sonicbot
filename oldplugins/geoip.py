bb43bdf08af2e6aa14fe60f68ee4a0c25a59a0fe
Westly Ward <sonicrules1234@gmail.com>
2010-11-17 11:34:10 -0800

    Fixing a problem in geoip related to unicode

--- oldplugins/geoip.py
+++ oldplugins/geoip.py
@@ -5,6 +5,6 @@ helpstring = "geoip <IP or hostname>"
 minlevel = 1
 
 def main(connection, info, args, world) :
-    IPInfo = pyipinfodb.IPInfo(world.globalsettings["IPInfoDB-apikey"])
+    IPInfo = pyipinfodb.IPInfo(str(world.globalsettings[u"IPInfoDB-apikey"]))
     if args[1] != "" : connection.msg(info["channel"], "%s: %s" % (info["sender"], repr(IPInfo.GetCity(args[1]))))
     else : connection.msg(info["channel"], "You need to specify a host.")
