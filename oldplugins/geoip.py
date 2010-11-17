8136db27a06e215fbd5907895bc7a9a43e9d4dfc
Westly Ward <sonicrules1234@gmail.com>
2010-11-17 11:47:45 -0800

    Making the geoip plugin work with an IP or a host for input

--- oldplugins/geoip.py
+++ oldplugins/geoip.py
@@ -1,4 +1,4 @@
-import imp
+import imp, socket
 pyipinfodb = imp.load_source("pyipinfodb", "pyipinfodb/pyipinfodb.py")
 arguments = ["self", "info", "args", "world"]
 helpstring = "geoip <IP or hostname>"
@@ -6,5 +6,5 @@ minlevel = 1
 
 def main(connection, info, args, world) :
     IPInfo = pyipinfodb.IPInfo(str(world.globalsettings[u"IPInfoDB-apikey"]))
-    if args[1] != "" : connection.msg(info["channel"], "%s: %s" % (info["sender"], repr(IPInfo.GetCity(args[1]))))
+    if args[1] != "" : connection.msg(info["channel"], "%s: %s" % (info["sender"], repr(IPInfo.GetCity([cur[4][0] for cur in socket.getaddrinfo(args[1],None) if cur[0]==2][0]))))
     else : connection.msg(info["channel"], "You need to specify a host.")
