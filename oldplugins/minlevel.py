c0277c4c1c4283d382f0b710b3e50f57ae890e5f
Westly Ward <sonicrules1234@gmail.com>
2011-05-05 10:43:46 -0700

    Fixing nickreg and minlevel

--- oldplugins/minlevel.py
+++ oldplugins/minlevel.py
@@ -2,7 +2,7 @@ minlevel = 1
 helpstring = "minlevel <command>"
 arguments = ["self", "info", "args", "world"]
 
-def main(connection, info, args, plugins) :
+def main(connection, info, args, world) :
     """Returns the minimum user level for a plugin"""
     if world.plugins.has_key(args[1]) :
         connection.msg(info["channel"], _("The minimum user level needed for that plugin is: %(level)s.") % dict(level=world.plugins[args[1]]["minlevel"]))
--- oldplugins/nickreg.py
+++ oldplugins/nickreg.py
@@ -6,7 +6,7 @@ minlevel = 1
 def main(connection, info, args) :
     """Registers the sender with sonicbot"""
     if connection.users["users"][info["sender"]]["userlevel"] == 1 :
-        salt = self.gensalt()
+        salt = connection.gensalt()
         connection.users["users"][info["sender"]] = {"salt":salt,"password":hashlib.sha512(args[1]+salt).hexdigest(), "hostname":[connection.hostnames[info["sender"]]], "userlevel":2}
         connection.users.sync()
         connection.msg(info["sender"], _("You have registered the nick '%(nick)s' with password '%(password)s' and hostname '%(hostname)s'.  Congratulations!  You now have a user level of 2!") % dict(nick=info["sender"], password=args[1], hostname=connection.hostnames[info["sender"]]))
