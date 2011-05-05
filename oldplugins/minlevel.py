6ccc8acc16094d57e050132869047a7381d483f6
Westly Ward <sonicrules1234@gmail.com>
2011-05-05 10:46:25 -0700

    Fixing another error in minlevel

--- oldplugins/minlevel.py
+++ oldplugins/minlevel.py
@@ -5,5 +5,5 @@ arguments = ["self", "info", "args", "world"]
 def main(connection, info, args, world) :
     """Returns the minimum user level for a plugin"""
     if world.plugins.has_key(args[1]) :
-        connection.msg(info["channel"], _("The minimum user level needed for that plugin is: %(level)s.") % dict(level=world.plugins[args[1]]["minlevel"]))
+        connection.msg(info["channel"], _("The minimum user level needed for that plugin is: %(level)s.") % dict(level=world.plugins[args[1]][0]["minlevel"]))
     else : connection.msg(info["channel"], _("No such plugin."))
