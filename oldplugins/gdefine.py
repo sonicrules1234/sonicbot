7ca9fb9997fa08778a2e975a55dc95e271405de4
Westly Ward <sonicrules1234@gmail.com>
2011-07-29 16:01:44 -0700

    This should fix gdefine

--- oldplugins/gdefine.py
+++ oldplugins/gdefine.py
@@ -12,7 +12,7 @@ def gdefine(word):
     req=urllib2.Request("http://www.google.com/search?%s"%urllib.urlencode({'q':'define:'+word,'oe':'utf-8','ie':'utf-8'}))
     req.add_header('User-agent','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.14) Gecko/2009090216 Ubuntu/9.04 (jaunty) Firefox/3.0.14')
     #return re.sub('&([^;]+);',lambda found: {'quot':'"',"amp":'&'}[found.groups()[0]],re.findall('<li>([^<]+)',urllib2.build_opener().open(req).read())[0])
-    regex1 = re.findall("""<div class=s><div>(.*)\&nbsp\;\&nbsp\;<a class=fl href="/search\?""", urllib2.build_opener().open(req).read())
+    regex1 = re.findall("""<div class=s><div><span class=f>.*</span>(.*)\&nbsp\;\&nbsp\;<a class=fl href="/search\?""", urllib2.build_opener().open(req).read())
     #return re.findall("""
     return re.sub("^\d+\. ", "", regex1[0].split("</div><div>")[0])
 def partfilter(inputtext) :
