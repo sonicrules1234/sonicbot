a75c3903193b508f42cd6ab728dd5ac6253498c4
Westly Ward <sonicrules1234@gmail.com>
2011-12-07 12:15:48 -0800

    Fixing a spelling error

--- oldplugins/chlvl.py
+++ oldplugins/chlvl.py
@@ -13,5 +13,5 @@ def main(connection, info, args) :
                 self.ignorelist.append(args[3])
                 connection.msg(info["channel"], len(fnmatch.filter(connection.channellist[info["channel"]], args[3])) + " users ignored with this mask")
             elif connection.users["users"][args[1]]["userlevel"] == 0 and int(args[2]) != 0:
-                self.ignorelist.remove(args[3])
+                connection.ignorelist.remove(args[3])
     else : connection.msg(info["channel"], _("No such user in my database."))
