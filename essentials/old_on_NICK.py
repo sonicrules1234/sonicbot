6b38b91f1f917e54a0da0953f961bb9129c9cf77
Westly Ward <sonicrules1234@gmail.com>
2011-12-07 12:51:35 -0800

    Making sonicbot keep track with the whoislist variable on nick changes

--- essentials/old_on_NICK.py
+++ essentials/old_on_NICK.py
@@ -8,6 +8,7 @@ def main(self, info) :
         newnick = info["words"][2][1:]
     else : newnick = info["words"][2]
     self.hostnames[newnick] = info["hostname"]
+    self.whoislist[newnick] = info["whois"]
     if not self.debug : print _("[%(time)s]**%(oldnick)s is now known as %(newnick)s") % dict(time=time.strftime("%b %d %Y, %H:%M:%S %Z"), oldnick=info["sender"], newnick=info["words"][2][1:])
     for channel in self.channellist :
         if info["sender"] in self.channellist[channel] :
