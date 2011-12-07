68c36511a97316fa36685ab2178da0f9e6269579
Westly Ward <sonicrules1234@gmail.com>
2011-12-07 12:36:23 -0800

    Making it so sonicbot tracks full user masks

--- essentials/old_on_352.py
+++ essentials/old_on_352.py
@@ -4,3 +4,4 @@ arguments = ["self", "info"]
 keyword = "352"
 def main(self, info) :
     self.hostnames[info["words"][7]] = info["words"][5]
+    self.whoislist[info["words"][7]] = info["words"][7] + "!" + info["words"][4] + "@" + info["words"][5]
