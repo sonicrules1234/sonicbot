f95ae0d1fa968a9727468e18c9987cac8a368f0f
Westly Ward <sonicrules1234@gmail.com>
2012-04-02 12:05:16 -0700

    Making sonicbot ignore memos from the same person if they are the same as the last memo from that person

--- oldplugins/memo.py
+++ oldplugins/memo.py
@@ -10,8 +10,12 @@ def main(connection, info, args) :
         db[args[1].lower()] = []
         db.sync()
     if len(db[args[1].lower()]) < 3 :
-        db[args[1].lower()].append({"sender":info["sender"], "message":" ".join(args[2:]), "time":time.time()})
-        db.sync()
-        db.close()
-        connection.ircsend(info["channel"], "%(sender)s: Message sent." % dict(sender=info["sender"]))
+        if len(db[args[1].lower()) > 0 :
+            if " ".join(args[2:]) == db[args[1].lower()][-1]["message"] and info["sender"] == db[args[1].lower()][-1]["sender"] :
+                connection.ircsend(info["channel"], "%(sender)s: Ignoring repeated message." % dict(sender=info["sender"]))
+        else :
+            db[args[1].lower()].append({"sender":info["sender"], "message":" ".join(args[2:]), "time":time.time()})
+            db.sync()
+            db.close()
+            connection.ircsend(info["channel"], "%(sender)s: Message sent." % dict(sender=info["sender"]))
     else : connection.ircsend(info["channel"], "Sorry, but that person already has 3 memos.")
