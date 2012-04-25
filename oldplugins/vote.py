afc477eaad24c66a0b3bc1cac250120818e77f7e
Westly Ward <sonicrules1234@gmail.com>
2012-04-25 11:27:52 -0700

    Fixing a bug in vote.py that is caused by a feature of python

--- oldplugins/vote.py
+++ oldplugins/vote.py
@@ -7,12 +7,14 @@ def main(connection, info, args, world) :
     """Lets user vote"""
     votes = shelve.open("votes.db", writeback=True)
     if votes["networks"][connection.networkname][int(args[1]) - 1]["started"] :
-        if info["sender"] not in votes["networks"][connection.networkname][int(args[1]) - 1]["voters"] :
+        if info["sender"] not in votes["networks"][connection.networkname][int(args[1]) - 1]["voters"] and ((int(args[1]) - 1) >= 1) :
             votes["networks"][connection.networkname][int(args[1]) - 1]["choices"][int(args[2]) - 1]["votes"].append(info["sender"])
             votes.sync()
             votes["networks"][connection.networkname][int(args[1]) - 1]["voters"].append(info["sender"])
             votes.sync()
             connection.msg(info["sender"], _("You have successfully voted '%(vote)s' on '%(topic)s'.") % dict(vote=votes["networks"][connection.networkname][int(args[1]) - 1]["choices"][int(args[2]) - 1]["choice"], topic=votes["networks"][connection.networkname][int(args[1]) - 1]["topic"]))
+        elif (int(args[1]) -1) < 1 :
+            connection.msg(info["channel"], "Invalid vote!", True)
         else : connection.msg(info["channel"], _("%(sender)s: You have already voted!") % dict(sender=info["sender"]))
     else : connection.msg(info["channel"], _("That vote has not yet started!"))
     votes.close()
