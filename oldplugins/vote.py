2c8c02a45f700f37eebc70b7bc079aedac3de123
Westly Ward <sonicrules1234@gmail.com>
2012-05-17 09:08:54 -0700

    Fixing a mistake I made in vote.py

--- oldplugins/vote.py
+++ oldplugins/vote.py
@@ -7,13 +7,13 @@ def main(connection, info, args, world) :
     """Lets user vote"""
     votes = shelve.open("votes.db", writeback=True)
     if votes["networks"][connection.networkname][int(args[1]) - 1]["started"] :
-        if info["sender"] not in votes["networks"][connection.networkname][int(args[1]) - 1]["voters"] and ((int(args[1]) - 1) >= 1) :
+        if info["sender"] not in votes["networks"][connection.networkname][int(args[1]) - 1]["voters"] and (int(args[1]) >= 1) and ((int(args[2]) - 1) >= 0) :
             votes["networks"][connection.networkname][int(args[1]) - 1]["choices"][int(args[2]) - 1]["votes"].append(info["sender"])
             votes.sync()
             votes["networks"][connection.networkname][int(args[1]) - 1]["voters"].append(info["sender"])
             votes.sync()
             connection.msg(info["sender"], _("You have successfully voted '%(vote)s' on '%(topic)s'.") % dict(vote=votes["networks"][connection.networkname][int(args[1]) - 1]["choices"][int(args[2]) - 1]["choice"], topic=votes["networks"][connection.networkname][int(args[1]) - 1]["topic"]))
-        elif (int(args[1]) -1) < 0 :
+        elif (int(args[2]) -1) < 0 :
             connection.msg(info["channel"], "Invalid vote!", True)
         else : connection.msg(info["channel"], _("%(sender)s: You have already voted!") % dict(sender=info["sender"]))
     else : connection.msg(info["channel"], _("That vote has not yet started!"))
